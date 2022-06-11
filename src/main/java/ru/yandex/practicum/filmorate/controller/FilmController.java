8658dc926df6b27f95b21fb484b69e044376413b
grinevs <accsergey@gmail.com>
2022-06-11 15:52:31 +0300

    update controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,24 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 
 @RestController
 public class FilmController {
+    @PostMapping(value = "/films")
+    public Film addFilm(@RequestBody Film film) {
+
+        return film;
+    }
+
+    @PatchMapping(value = "/films")
+    public Film patchFilm(@RequestBody Film film) {
+
+        return film;
+    }
+
+    @GetMapping("/films")
+    public void getAllFilms() {
+
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,22 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
 
 @RestController
 public class UserController {
+    @PostMapping(value = "/users")
+    public User create(@RequestBody User user) {
+        return user;
+    }
+
+    @PatchMapping(value = "/users")
+    public User patchUser(@RequestBody User user)  {
+        return user;
+    }
+
+    @GetMapping("/users")
+    public void getUsers() {
+
+    }
 }
